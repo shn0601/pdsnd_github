6374ba8e78153462fa235e171d63d39abcacfc20
Hanna Chang <hannac@hannac-macbookpro2.roam.corp.google.com>
2019-12-30 08:32:56 -0800

    I did not learn Python as part of this Udacity nanodegree program

--- bikeshare.py
+++ bikeshare.py
@@ -26,6 +26,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!!')
+    print('I did not learn Python as part of this Udacity nanodegree program')
 
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
